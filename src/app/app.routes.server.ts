59153412afd5e342ac60ae85a80cfb8f95fa8a57
Apple <140983808+kareem732@users.noreply.github.com>
2026-05-21 06:05:34 +0300

    fix build errors

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'instructor/my-courses/:id/edit',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'student/courses/continue-learning/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'student/profile/my-learnings/course-learning/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
